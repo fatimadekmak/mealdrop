e28f0b60d48ba81d05b6fc3a83901752e78b5c26
fatimadekmak <fatima.k.dokmak@gmail.com>
2023-03-01 13:50:06 +0200

    create admin user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,25 @@
 <?php
 
-namespace App\Http\Controllers;
 
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+
 
 class HomeController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('home');
+    }
+
+    public function redirects()
+    {
+        $usertype = Auth::user()->user_type;
+
+        if ($usertype == '1')
+            return view('admin.adminhome');
+        else if ($usertype == '0')
+            return view('home');
     }
 }
